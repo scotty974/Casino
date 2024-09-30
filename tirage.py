27affaebc126e8bcd6249859511daf9341361b1c
scotty974 <fabienetheve@hotmail.com>
2024-09-30 14:07:56 +0200

    add message debug

--- tirage.py
+++ tirage.py
@@ -5,7 +5,7 @@ def random_number(level):
         nb_try += 2
         random_nb = random.randrange(1, 10+10)
         if i == level -1:
-            print(nb_try, random_nb)
+            return {'nb_try' : nb_try, 'random_nb' : random_nb}
+        else : 
+            print('Rentrez un niveau valide !')
         
-
-random_number(1)
